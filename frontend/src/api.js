f0e4d8905caf7d09f470bd1c0fdb23b0b7fa3315
Manoj Baratam <manojbaratam02@example.com>
2026-04-23 14:06:34 +0530

    connect deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
-
+const API = "https://aimonk-nested-tree.onrender.com";
 export const getTrees = () => axios.get(`${API}/tree`);
 export const createTree = (data) => axios.post(`${API}/tree`, data);
 export const updateTree = (id, data) =>
